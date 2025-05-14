c5b46c4aaf1287b74881294eae93b0ca8f3d5cb0
gaesigua <67262987+gaesigua@users.noreply.github.com>
2025-05-14 12:50:48 +0200

    first commit

---
+++ src/main/java/com/gasigwatin/REST_3_Springboot_project/Rest3SpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gasigwatin.REST_3_Springboot_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rest3SpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rest3SpringbootProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gasigwatin/REST_3_Springboot_project/Rest3SpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gasigwatin.REST_3_Springboot_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rest3SpringbootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
